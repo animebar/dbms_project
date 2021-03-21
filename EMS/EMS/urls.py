c382132e55dcbfd8f75376e786ad77573d28cf17
Kushal <kushalnatani123@gmail.com>
2021-03-22 00:15:35 +0530

    added home page frontend

--- EMS/EMS/urls.py
+++ EMS/EMS/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('user/', include('user.urls')),
+    path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
