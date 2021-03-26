7541c4acdbba7df7d41a0f91f296b0b10803877a
iamCodeMonk <animeshbaruah7@gmail.com>
2021-03-27 02:39:30 +0530

    host_events

--- EMS/events/urls.py
+++ EMS/events/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     path('view/event_id=<id>', views.view_event, name='view_event'),
+    path('host', views.host_event, name='host_event'),
 ]
